5ab28d1663e78c42947bdd4f37cc590089c5be04
wojnoski <karol.wojnarowski@gmail.com>
2023-02-13 12:21:51 +0100

    Active a clicked element

--- js/script.js
+++ js/script.js
@@ -5,22 +5,26 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
 */
 const titleClickHandler = function(event){
-    console.log(event);
+    const clickedElement = this;
+    console.log('clickedElement (with plus): ' + clickedElement);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts article.active');
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
+    /*const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
-    }
+    }*/
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
