d51481a14b8d500adeca5d002d4759b3ee333ab8
wojnoski <karol.wojnarowski@gmail.com>
2023-02-13 12:43:20 +0100

    Prevent default

--- js/script.js
+++ js/script.js
@@ -7,24 +7,23 @@ document.getElementById('test-button').addEventListener('click', function(){
 const titleClickHandler = function(event){
     const clickedElement = this;
     console.log('clickedElement (with plus): ' + clickedElement);
+    event.preventDefault();
   
-    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-    
+
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
 
-    /* [DONE] remove class 'active' from all articles */
     /*const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }*/
+
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
